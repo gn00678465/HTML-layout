7305010062171cd95d7b6892ff7a33279bef10a7
MADAO <gn00678465@gmail.com>
2020-11-12 21:45:10 +0800

    test js

--- week4/app/assets/js/all.js
+++ week4/app/assets/js/all.js
@@ -1,5 +1,15 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+// $(document).ready(() => {
+//   const tabs = [...document.querySelectorAll('.tab')];
+//   function tabHandler() {
+//     const currentPath = window.location.pathname.split('.')[0].replace(/[^a-zA-Z]/g, '');
+//     const currentTab = this.dataset.tab;
+//     if (currentPath === currentTab) {
+//       this.classList.add('active')
+//     }
+//   };
+//   tabs.forEach((tab) => {
+//     const link = tab.querySelector('a');
+//     link.classList.remove('active');
+//     link.addEventListener('click', tabHandler);
+//   });
+// });
--- week4/dist/assets/js/all.js
+++ week4/dist/assets/js/all.js
@@ -1,7 +1,17 @@
+// $(document).ready(() => {
+//   const tabs = [...document.querySelectorAll('.tab')];
+//   function tabHandler() {
+//     const currentPath = window.location.pathname.split('.')[0].replace(/[^a-zA-Z]/g, '');
+//     const currentTab = this.dataset.tab;
+//     if (currentPath === currentTab) {
+//       this.classList.add('active')
+//     }
+//   };
+//   tabs.forEach((tab) => {
+//     const link = tab.querySelector('a');
+//     link.classList.remove('active');
+//     link.addEventListener('click', tabHandler);
+//   });
+// });
 "use strict";
-
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
 //# sourceMappingURL=all.js.map
